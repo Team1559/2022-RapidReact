94385986c25099625fe31e772017c07110bd9144
Anna-Marchetti <91164331+Anna-Marchetti@users.noreply.github.com>
2022-01-18 19:24:18 -0500

    Added all of the shooter fields

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,12 +1,37 @@
 package frc.robot.subsystems;
 
 import frc.robot.OperatorInterface;
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.can.*;
+import edu.wpi.first.wpilibj.Solenoid;
+import frc.robot.*;
 
 public class Shooter {
+    
     private OperatorInterface oi;
     private double shooter_kF = 0; 
-    private double shooter_kP = 5;
+    private double shooter_kP = .007;
     private double shooter_kD = 0;
     private double shooter_kI = 0.00000;// 1e-6
-
+    //private SupplyCurrentLimitConfiguration shooterLimit = new SupplyCurrentLimitConfiguration(true, 100, 20, 1000);
+    private TalonFX shooter;
+    private double shooterRpms = 98;
+    public void init(OperatorInterface operatorinterface){
+        oi = operatorinterface;
+        // Shooter Motor Config
+        shooter.set(TalonFXControlMode.Velocity, 0);
+        shooter.configClosedloopRamp(cLR, TIMEOUT);
+        shooter.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder); //shooter.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+        shooter.config_kF(0, shooter_kF);
+        shooter.config_kP(0, shooter_kP);
+        shooter.config_kD(0, shooter_kD);
+        shooter.config_kI(0, shooter_kI);
+        shooter.configNominalOutputForward(0, TIMEOUT);
+        shooter.configNominalOutputReverse(0, TIMEOUT);
+        shooter.configPeakOutputForward(+1, TIMEOUT);
+        shooter.configPeakOutputReverse(-1, TIMEOUT);
+        shooter.setNeutralMode(NeutralMode.Coast);
+        shooter.configSupplyCurrentLimit(shooterLimit);
+        
+    }
 }
