853324455f8ae558813275050464097d0f83ec6b
JordanCieslinski <jordan@cieslinskis.com>
2022-01-18 19:08:10 -0500

    added pid loops

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,3 +1,12 @@
+package frc.robot.subsystems;
+
+import frc.robot.OperatorInterface;
+
 public class Shooter {
-    
+    private OperatorInterface oi;
+    private double shooter_kF = 0; 
+    private double shooter_kP = 5;
+    private double shooter_kD = 0;
+    private double shooter_kI = 0.00000;// 1e-6
+
 }
