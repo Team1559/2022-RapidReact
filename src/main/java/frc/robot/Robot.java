7a683c096ca83076c48a4b2abbd45f0f77d9683f
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-01-20 00:50:48 +0000

    Latest Vision framework (#10)
    * Initial commit
    
    * updated for vision stuff
    
    * Updated for the rest of the new vision stuff
    
    * removed the unnecessary print
    
    * added Dounce check for the Dpad
    
    * More framework
    
    * More framework
    
    * added wait for other robot variable
    
    * finished framework
    
    * updated to latest wpilib
    
    * added more sudo code
    
    * Initial Climber commit
    
    Make existing code more efficient and readable, add a Climber subsystem, and add a method to square while keeping the sign
    
    * Applied a consistent formatting scheme
    
    Created a formatter profile for our repo (formatter-settings.xml) and included it in the repo. Now, anyone who uses the format command from the command palette will automatically apply a consistent profile across all java files.
    
    * Fixed problems and stuff
    
    * Moved away from constants class to avoid merge errors later on
    
    * formatted the document
    
    * resolved the errors
    
    Co-authored-by: MathNerd28 <89086035+MathNerd28@users.noreply.github.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,25 +5,29 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import frc.robot.subsystems.Vision;
-import frc.robot.components.VisionData;
-
+import frc.robot.subsystems.*;
+import frc.robot.components.*;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
-  
 
-  public OperatorInterface oi = new OperatorInterface();
-  public Vision vision;
-  VisionData vData;
+  private OperatorInterface oi = new OperatorInterface();
+  // auto classes
+  private VisionControl visionControl;
+  private Vision vision;
+  private VisionData vData;
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
@@ -31,23 +35,35 @@ public class Robot extends TimedRobot {
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and
+   * test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+  }
 
   /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different
+   * autonomous modes using the dashboard. The sendable chooser code works with
+   * the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
+   * chooser code and
+   * uncomment the getString line to get the auto name from the text box below the
+   * Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure
+   * below with additional strings. If using the SendableChooser make sure to add
+   * them to the
    * chooser code above as well.
    */
   @Override
@@ -58,9 +74,8 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    if(FeatureFlags.doVision && FeatureFlags.visionInitalized){
-      vData = vision.getData();
-      vData.Print();
+    if (FeatureFlags.doVision && FeatureFlags.visionInitalized) {
+      visionControl.auto();
     }
   }
 
@@ -73,34 +88,36 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    if(FeatureFlags.doVision && FeatureFlags.visionInitalized){
-      vData = vision.getData();
-      vData.Print();
+    if (FeatureFlags.doVision && FeatureFlags.visionInitalized) {
+      visionControl.main();
     }
   }
-
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   /** This function is called once when test mode is enabled. */
   @Override
-  public void testInit() {}
+  public void testInit() {
+  }
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
-  public void initialize(){
-    if(FeatureFlags.doVision && !FeatureFlags.visionInitalized){
+  public void initialize() {
+    if (FeatureFlags.doVision && !FeatureFlags.visionInitalized) {
       vision = new Vision();
       vision.VisionInit();
+      visionControl = new VisionControl(vision, vData, oi);// chassis, shooter);
       FeatureFlags.visionInitalized = true;
     }
-
   }
 }
--- src/main/java/frc/robot/components/VisionData.java
+++ src/main/java/frc/robot/components/VisionData.java
@@ -1,7 +1,7 @@
 package frc.robot.components;
 
 public class VisionData {
-    
+
     public double hx;
     public double hy;
     public double hr;
@@ -10,23 +10,28 @@ public class VisionData {
     public double br;
     public Integer hoopStatus;
     public Integer ballStatus;
+    public boolean waitForOtherRobot;
 
-    public boolean isHoopValid(){
-        if(hoopStatus!=1){
+    public boolean isHoopValid() {
+        if (hoopStatus != 1) {
             return false;
-        }
-        else
+        } 
+        else {
             return true;
+        }
     }
-    public boolean isBallValid(){
-        if(ballStatus!=1){
+
+    public boolean isBallValid() {
+        if (ballStatus != 1) {
             return false;
-        }
-        else
+        } 
+        else{
             return true;
+        }
     }
 
     public void Print() {
-        System.out.printf("%3.1f %3.1f %3.1f %3.1f %3.1f %3.1f %d %d\n", hx, hy, hr, bx, by, br, hoopStatus, ballStatus);
+        System.out.printf("%3.1f %3.1f %3.1f %3.1f %3.1f %3.1f %d %d\n", hx, hy, hr, bx, by, br, hoopStatus,
+                ballStatus);
     }
 }
