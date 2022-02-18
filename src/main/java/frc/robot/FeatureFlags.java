dff742d5f1be7275273d5d1b23b7322beb3dcb82
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-02-18 08:19:18 -0500

    made update dependencies smarter

--- src/main/java/frc/robot/FeatureFlags.java
+++ src/main/java/frc/robot/FeatureFlags.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+@SuppressWarnings("unused")
 public final class FeatureFlags {
 
     // Modify these values
@@ -9,28 +10,49 @@ public final class FeatureFlags {
     public static boolean doShooter = false;
     public static boolean doCompressor = false;
 
+    // Set these once the subsystem in installed
+    public static final boolean VISION_INSTALLED = true;
+    public static final boolean CHASSIS_INSTALLED = true;
+    public static final boolean IMU_INSTALLED = true;
+    public static final boolean SHOOTER_INSTALLED = false;
+    public static final boolean COMPRESSOR_INSTALLED = false;
+
     // Leave these values alone
     public static boolean visionInitialized = false;
-    public static boolean chassisInitialized  = false;
+    public static boolean chassisInitialized = false;
     public static boolean imuInitialized = false;
     public static boolean shooterInitalized = false;
     public static boolean compressorInitialized = false;
 
-
     public static void updateDependencies() {
-        if(doVision) {
-            doChassis= true;
-            doImu = true;
-            // doShooter = true;
-            // doCompressor = true;
-        }
-        
-        if(doChassis) {
-            doImu = true;
+        if (doVision && VISION_INSTALLED) {
+            if (CHASSIS_INSTALLED) {
+                doChassis = true;
+            }
+
+            if (IMU_INSTALLED) {
+                doImu = true;
+            }
+
+            if (SHOOTER_INSTALLED) {
+                doShooter = true;
+            }
+
+            if (COMPRESSOR_INSTALLED) {
+                doCompressor = true;
+            }
         }
 
-        if(doShooter){
-            doCompressor = true;
+        if (doChassis && CHASSIS_INSTALLED) {
+            if (IMU_INSTALLED) {
+                doImu = true;
+            }
+        }
+
+        if (doShooter && SHOOTER_INSTALLED) {
+            if (COMPRESSOR_INSTALLED) {
+                doCompressor = true;
+            }
         }
     }
 }
