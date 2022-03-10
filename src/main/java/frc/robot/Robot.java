479130409bef874b2ca3a5b9ccfb8649d42832ff
Software <1559DevilTech@gmail.com>
2022-03-10 11:35:13 -0500

    green light

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -283,7 +283,9 @@ public class Robot extends TimedRobot {
     /** This function is called periodically when disabled. */
 
     @Override
-    public void disabledPeriodic() {}
+    public void disabledPeriodic() {
+        PDM.setSwitchableChannel(false);
+    }
 
     /** This function is called once when test mode is enabled. */
 
