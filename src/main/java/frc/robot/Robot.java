63ea71866a6bc8fd200e38347328fcac597dbc89
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-03-18 13:47:08 -0400

    when did that 3 get there?

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,7 +17,7 @@ import frc.robot.subsystems.*;
 import frc.robot.components.*;
 
 /**
- * The VM is configured to automatically run this class, and to cal3l the
+ * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the build.gradle file in the
