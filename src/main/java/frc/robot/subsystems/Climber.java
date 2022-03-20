0b65fa3a5cafdfabdc86706b4bed7c4a30be2f5a
Software <1559DevilTech@gmail.com>
2022-03-20 17:31:19 -0400

    changed Climber speed

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -28,8 +28,8 @@ public class Climber {
     private boolean disable = false;
     private boolean press = false;
 
-    private final double CLIMBER_DOWN_RPMS = 3000;
-    private final double CLIMBER_UP_RPMS = 2000;
+    private final double CLIMBER_DOWN_RPMS = 2500;
+    private final double CLIMBER_UP_RPMS = 1600;
 
     private boolean resetEncoder = true;
 
